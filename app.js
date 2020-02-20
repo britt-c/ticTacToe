7d19548475ef3c877555660e94cd9ca304feef4e
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2020-02-20 02:07:46 -0800

    hand to kind of delete some stuff and just start over on the appjs. So far I can click on a square and have the console tell me what square I clicked. But nothing changes with the square yet

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 window.onload = function() {
+	const allBoxes = document.querySelectorAll("button.btn")
+
+	allBoxes.forEach(function(boxes){
+		boxes.addEventListener("click", function(event){
+			let box = event.currentTarget
+			console.log(box)
+		})
+	})
+	 
 }
