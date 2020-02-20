814f781c7b687aef47321ea70092be65819ec289
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2020-02-20 03:20:59 -0800

    okay okay, figured out how to get an X or an O to insert depending on the one or double click. Bug is you can change it still.

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 window.onload = function() {
 	const allBoxes = document.querySelectorAll("button.btn")
-			allBoxes.forEach(function(boxes){
-			boxes.addEventListener("click", function(event){
-				let box = event.currentTarget
-				console.log(box)
-				box.innerText = "x"
-			})
-		})
-	}
+
+	allBoxes.forEach(function(boxes){
+		boxes.addEventListener("click", onClick)
+		boxes.addEventListener("dblclick", onDblClick)
+	})
+
+function onClick() {
+	let box = event.currentTarget
+	box.innerText = "X"
+}
+
+
+function onDblClick() {
+	let box = event.currentTarget
+	box.innerText = "O"
+}
+
+}
