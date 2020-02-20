6be86a07c4154f4b2d054a647f2c0ef8dc495249
Brittney Cortez <brittneycortez@Brittneys-MacBook-Pro.local>
2020-02-20 05:08:10 -0800

    was able to add some fun stuff to my page. cant clear yet and can still change button status

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 window.onload = function() {
 	const allBoxes = document.querySelectorAll("button.btn")
+	const restart = document.
+	document.getElementById("output").addEventListener("mouseover", easterEgg)
+	document.getElementById("output").addEventListener("mouseout", displayHeader)
 	
 	allBoxes.forEach(function(boxes){
 		boxes.addEventListener("click", onClick)
@@ -19,6 +22,25 @@ function onDblClick() {
 	box.innerText = "O"
 	box.style.color= "black"
 	box.style.background = "red"
+}
+
+function displayHeader(){
+	const header = "Tic Tac Toe"
+	const output = document.querySelector("#output")
+	output.innerText = header
+
+}
+
+displayHeader()
+
+function easterEgg(){
+	const egg = "Rock Paper Scissors"
+	const output = document.querySelector("#output")
+	output.innerText = egg
+}
+
+function reset(){
+		
 }
 
 }
